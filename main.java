2448ef6da4e67491e49ac4a08f7ddfe326f1887c
Sujita786-star <sujitakc41@gmail.com>
2026-08-20 15:23:49 +0545

    Create main.java

---
+++ main.java
@@ -0,0 +1,21 @@
+package restaurantmenumanager;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("===== RESTAURANT MENU MANAGER =====");
+
+        System.out.println();
+        System.out.println("1. Add Menu Item");
+        System.out.println("2. View Menu");
+        System.out.println("3. Search Menu Item");
+        System.out.println("4. Update Menu Item");
+        System.out.println("5. Delete Menu Item");
+        System.out.println("6. Evaluate Menu Item");
+        System.out.println("7. View Feedback");
+        System.out.println("8. Reward/Penalty");
+        System.out.println("9. Save Data");
+        System.out.println("10. Exit");
+    }
+}
